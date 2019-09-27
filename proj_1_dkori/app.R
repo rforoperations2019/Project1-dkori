849ba5fcc206cb26ae3ad66df323c044d31daae7
dkori <dave.kori@gmail.com>
2019-09-27 19:55:16 -0400

    Added Seasonality chart, improved comments

--- proj_1_dkori/app.R
+++ proj_1_dkori/app.R
@@ -39,11 +39,13 @@ guns<-read_csv(remote_data)%>%
 guns$date<-paste(guns$year,guns$month,'01',sep=' ')
 #create a datetime variable out of the date variable
 guns$datetime<-as.Date(guns$date,format='%Y %m %d')
+#Make Police involvement categorical
 guns$`Police Involved?`=ifelse(guns$police==1,"Yes","No")
+#expand name for sex
 guns$Sex<-ifelse(guns$sex=="M","Male","Female")
 test<-max(guns$datetime,na.rm=TRUE)-min(guns$datetime,na.rm=TRUE)
 
-header <- dashboardHeader(title = "Gun Violence Deaths: 2012-2014"
+header <- dashboardHeader(title = "Gun Violence Deaths"
 )
 
 # Dashboard Sidebar ----------------------------------------------
@@ -87,7 +89,7 @@ body <- dashboardBody(tabItems(
  
             valueBoxOutput("deaths"),
             valueBoxOutput("timespan"),
-            valueBoxOutput("deaths_per_day"),
+            valueBoxOutput("deaths_per_month"),
           
           # Plot ----------------------------------------------
           #fluidRow(
@@ -104,6 +106,7 @@ body <- dashboardBody(tabItems(
             tabBox(title = "Breakdown",
                    width = 12,
                    tabPanel("By Age", plotlyOutput(outputId = "bar_plot")),
+                   tabPanel("Seasonality",plotlyOutput(outputId="seasonality")),
                    tabPanel("Overall Share",plotOutput(outputId="donut")))
           
   ),
@@ -125,30 +128,37 @@ server <- function(input, output) {
   series_choice<-reactive({paste0(input$series_choice)})
   #first subset of guns, whether or not suicides are included
   guns_subset<-reactive({
+    #check if they have selected the "include suicides" checkbox
     subset1<-if(!input$Checkbox){
+      #not selected, remove suicides
       guns%>%
         filter(intent!="Suicide")
     }else{
+      #if selected, keep guns the same
       guns
     }
-    subset2<-subset1[,c("datetime", "age",series_choice())]%>%
+    subset2<-subset1[,c("datetime","month", "age",series_choice())]%>%
       #limit subset to chosen dates
       #start date
       filter(datetime>=input$start_date[1])%>%
       #end date
       filter(datetime<=input$start_date[2])
     #rename columns (only way I know how to do this when selecting column through var)
-    names(subset2)<-c("datetime","age", "series_choice")
+    names(subset2)<-c("datetime","month", "age", "series_choice")
     return(subset2)
     })
 
   
   output$datatab<-DT::renderDataTable(
     DT::datatable(guns_subset()%>%
+                    #count rows for chosen series
                     group_by(series_choice)%>%
                     count(name="value")%>%
+                    #undue grouping
                     ungroup()%>%
+                    #have deaths display as comma
                     mutate(Deaths=comma(value))%>%
+                    #change name of series_choice
                     mutate(" " = series_choice)%>%
                     dplyr::select(` `, `Deaths`)
                   ,
@@ -160,19 +170,20 @@ server <- function(input, output) {
 
     #create the line plot using ggplot2
     lp<-guns_subset()%>%
+      #count observations by date
       group_by(datetime,series_choice)%>%
       count(name="value")%>%
+      #create line plot
       ggplot(aes(x=datetime,y=value,color=series_choice))+
       geom_line()+
+      #remove grey background
       theme_minimal()+
+      #label chart
       labs(x="Month",y="Number of fatalities",color=names(input$series_choice),
-           title="Gun Deaths Over Time")
+           title="Gun Deaths Over Time")+
+      scale_y_continuous(label=comma)
     lp
     
-      # mtcars%>%
-      # ggplot(aes(x=mpg,y=cyl))+
-      # geom_point()
-
   })
   
   
@@ -181,6 +192,7 @@ server <- function(input, output) {
 
     guns_subset()%>%
       group_by(series_choice)%>%
+      #role up by chosen series
       count(name="value")%>%
       ungroup()%>%
       mutate(ymax=cumsum(value),ymin=c(0,head(ymax,n=-1)))%>%
@@ -214,44 +226,80 @@ server <- function(input, output) {
       labs(title="Share of Gun Deaths by Category")
     
   })
-  
-  #Barplot by age
+
+    #Histogram by age
   output$bar_plot<-renderPlotly({
     
     
     gg_bar<-guns_subset()%>%
+      #role up by chosen series
       group_by(age,series_choice)%>%
       count(name="value")%>%
       ungroup()%>%
+      #ggplot histogram
       ggplot(aes(x=age,y=value,fill=series_choice))+
       geom_bar(stat="identity",position="stack")+
+      #remove grey background
       theme_minimal()+
+      #add labels
       labs(title="Deaths by Age",
            fill="",
-           y="fatalities")
+           y="fatalities")+
+      #add columsn to y axis label
+      scale_y_continuous(label=comma)
     gg_bar
+  })
+  
+  #seasonality chart
+  output$seasonality<-renderPlotly({
+    guns_subset()%>%
+      #create season vars
+      mutate(
+        #make month numeric so the case_when I already wrote will work
+        month<-as.numeric(month),
+        season=case_when(
+          month%in%c(12,1,2) ~ "Winter",
+          month%in%c(3,4,5) ~ "Spring",
+          month%in%c(6,7,8) ~ "Summer", 
+          month%in%c(9,10,11) ~ "Fall"))%>%
+      #role up by chosen series and by season
+      group_by(season,series_choice)%>%
+      count(name="value")%>%
+      ungroup()%>%
+      ggplot(aes(x=season,y=value,fill=series_choice))+
+      geom_bar(position="stack", stat="identity")+
+      scale_y_continuous(label=comma)+
+      #label axes
+      labs(title="Seasonal Breakdown",
+           x="Season",
+           y="Percentage of Deaths",
+           fill="")+
+      theme_minimal()
+    
   })
   
   #value and infoboxes
   #total deaths value box
   output$deaths <- renderValueBox({
-    
+    #count total deaths
     num <- nrow(guns_subset())
-    
     valueBox(subtitle = "Total Deaths", value = comma(num), icon = icon("cross",lib="font-awesome"), color = "red")
   })
   #time-span covered valuebox
   output$timespan <- renderValueBox({
     dat<-guns_subset()
+    #subtract first date by last date, divide by 30.5 to approximate months (since we don't know exact day)
     num <- as.numeric(max(dat$datetime)-min(dat$datetime))/30.5
     
     valueBox(subtitle = "Months Covered", value = comma(num), icon = icon("calendar"), color = "red")
   })
   
   #deaths/day
-  output$deaths_per_day <- renderValueBox({
+  output$deaths_per_month <- renderValueBox({
+    #count total deaths
     deaths<-nrow(guns_subset())
     dat<-guns_subset()
+    #count total months as before
     months<- as.numeric(max(dat$datetime)-min(dat$datetime))/30.5
     num<-(deaths/months)
     valueBox(subtitle = "Deaths per Month", value = round(num), icon = icon("heartbeat",lib="font-awesome"), color = "red")
